feeab62963bb9e43999ee0fce3bebe912b212e3b
Ugo Pattacini <ugo.pattacini@iit.it>
2016-12-31 21:36:05 +0100

    wip

--- scripts/gradebook.sh
+++ scripts/gradebook.sh
@@ -20,12 +20,20 @@ path=$2
 data=$path/data.json
 gradebook=$path/gradbook.json
 
+red='\033[1;31m'
+green='\033[1;32m'
+blue='\033[1;34m'
+cyan='\033[1;36m'
+nc='\033[0m'
+
 if [ ! -f "$data" ]; then
-    echo "Unable to find $data"
+    echo -e "${red}Unable to find ${data}${nc}\n"
     exit 2
 fi
 
 students=$(cat $data | jq '.students | .[]' | sed 's/\"//g')
-for student in $(students); do
-    echo $student
-done
+repositories=$(curl -s https://api.github.com/orgs/$org/repos?type=public | jq '.[] | .name' | sed 's/\"//g')
+
+echo -e "\nWorking out the students:\n${green}${students}${nc}\n"
+echo -e "Against the repositories:\n${blue}${repositories}${nc}\n"
+
