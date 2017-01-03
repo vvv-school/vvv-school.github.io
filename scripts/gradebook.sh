278fc9e3f4bd61cf149db2972a9e5dbb4ecff519
Ugo Pattacini <ugo.pattacini@iit.it>
2017-01-03 20:36:42 +0100

    wip

--- scripts/gradebook.sh
+++ scripts/gradebook.sh
@@ -156,7 +156,7 @@ function publish_gradebook {
             fi
             
             local totscore=$(eval "cat $cur_dir/student_data.tmp | jq '.score'")
-            echo -e "**Total Score = $totscore**" >> $README
+            echo -e "**Total Score = $totscore**\n" >> $README
             echo -e "---\n" >> $README
         done
         
