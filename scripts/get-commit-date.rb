87479e57816a48f3418c5cdc2c1a7e2b47076fdc
Ugo Pattacini <ugo.pattacini@iit.it>
2017-10-30 13:56:22 +0100

    Update get-commit-date.rb

--- scripts/get-commit-date.rb
+++ scripts/get-commit-date.rb
@@ -8,7 +8,7 @@
 # - octokit
 #
 # The env variable GITHUB_TOKEN_VVV_SCHOOL shall contain a valid GitHub token
-# (refer to instruction to find out more)
+# (refer to instructions to find out more)
 #
 # script for trasversing GitHub pagination
 #
