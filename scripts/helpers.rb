c0b84ad33a8a0fea74e2877dd79e6caa852f024b
Ugo Pattacini <ugo.pattacini@iit.it>
2021-10-01 09:23:57 +0200

    Update helpers.rb

--- scripts/helpers.rb
+++ scripts/helpers.rb
@@ -1,17 +1,18 @@
 # Copyright: (C) 2020 iCub Tech Facility - Istituto Italiano di Tecnologia
 # Authors: Ugo Pattacini <ugo.pattacini@iit.it>
 
-
+   
 #########################################################################################
 def check_and_wait_until_reset(client)
     rate_limit = client.rate_limit
     if rate_limit.remaining == 0 then
-        reset_secs = rate_limit.resets_in
+        reset_secs = rate_limit.resets_in + 60
         reset_mins = reset_secs / 60
         puts ""
-        puts "⏳ We hit the GitHub API rate limit; reset will occur at #{rate_limit.resets_at} in #{reset_mins} mins"
-        puts ""
-        reset_secs = reset_secs + 60
+        puts "⏳ We hit the GitHub API rate limit; reset will occur at #{rate_limit.resets_at}"
+        puts "⏳ Process suspended for #{reset_mins} mins"
         sleep(reset_secs)
+        puts "⏳ Process recovered ✔"
+        puts ""
     end
 end
