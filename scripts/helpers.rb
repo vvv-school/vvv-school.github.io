4bd5f9bad525bddcde1190fb9d04d88f72e20819
Ugo Pattacini <ugo.pattacini@iit.it>
2021-10-01 09:16:24 +0200

    Update helpers.rb

--- scripts/helpers.rb
+++ scripts/helpers.rb
@@ -8,7 +8,9 @@ def check_and_wait_until_reset(client)
     if rate_limit.remaining == 0 then
         reset_secs = rate_limit.resets_in
         reset_mins = reset_secs / 60
+        puts ""
         puts "⏳ We hit the GitHub API rate limit; reset will occur at #{rate_limit.resets_at} in #{reset_mins} mins"
+        puts ""
         reset_secs = reset_secs + 60
         sleep(reset_secs)
     end
